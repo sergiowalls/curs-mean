cd2381771971bf1d1f52fdea6de1936b541352f5
Sergio <sergio.paredes25@gmail.com>
2017-06-26 11:57:11 +0200

    Return array of messages

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ class Server {
         this.app = express();
         this.app.use(express.static("./public"));
         this.app.use("/helloworld", ((req, res) => res.status(200).json({message: "Hello world"})));
+        this.app.use("/api/quotes", ((req, res) => res.json([{message: "Foo"},{message: "Bar"}])));
         this.app.use((req, res, next) => res.status(404).json({message: "Resource not found"}))
     }
 
