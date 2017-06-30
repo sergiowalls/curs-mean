4ff376fd9077c2419a1f1b62851c6ec214d83522
Sergio <sergio.paredes25@gmail.com>
2017-06-30 12:36:21 +0200

    Fizzbuzz with typescript

--- web/app/main.ts
+++ web/app/main.ts
@@ -1,2 +1,16 @@
-let string: String = "hello";
-console.log(string);
+console.log(fizzbuzz());
+
+function fizzbuzz(): String {
+    let result: String = "";
+    for (let i: number = 0,
+             isFizz: boolean = false,
+             isBuzz: boolean = false;
+         i <= 100;
+         ++i, isFizz = i % 3 == 0, isBuzz = i % 5 == 0) {
+        result += isFizz ? "Fizz" : "";
+        result += isBuzz ? "Buzz" : "";
+        result += !isFizz && !isBuzz ? i.toString() : "";
+        result += "\n";
+    }
+    return result;
+}
